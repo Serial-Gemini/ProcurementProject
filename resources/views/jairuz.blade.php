d11836a5ae9c5363c320e40e6bd1b640828671e3
JairuzMari <marijairuz@gmail.com>
2026-07-21 00:24:06 +0800

    got mixed up i updated the old file lol mb

--- resources/views/jairuz.blade.php
+++ resources/views/jairuz.blade.php
@@ -7,10 +7,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Shantha Motors - Procurement System</title>
 
-//testing
-
-//test again for practice
-
     <style>
         * {
             box-sizing: border-box;

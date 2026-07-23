ed5b09428970220e8cd930a8bab250ac47901064
sense29-design <jayrillbulugagao09@gmail.com>
2026-07-23 13:10:23 +0800

    sense

--- resources/views/jay.blade.php
+++ resources/views/jay.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    // goods
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Purchase Order Management - Shantha Motors</title>
